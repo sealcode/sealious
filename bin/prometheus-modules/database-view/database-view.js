be21f953cf2bc987183b9e86a2a405337f5d97ec
simplypixi <pixiproject@gmail.com>
2014-11-25 14:48:25 +0100

    Issue #23

--- bin/prometheus-modules/database-view/database-view.js
+++ bin/prometheus-modules/database-view/database-view.js
@@ -1,5 +1,6 @@
 var Service = require("prometheus-service");
 var Database_access = require("prometheus-database-accessor");
+var resourceManager = require("prometheus-resource-manager");
 
 module.exports.register_services = function(){
 	return ["database-view"];
@@ -23,6 +24,11 @@ function construct_service(){
 			callback(response);
 		})
 	})
+	db_view_service.on("create", function(payload, callback){
+		resourceManager.newResource("test", {name: "testowy", kupa: "dupa"}, function(response){
+			callback(response);
+		})
+	})
 	return db_view_service;
 }
 
@@ -37,15 +43,31 @@ module.exports.service_object = function(service_name, dependencies){
 module.exports.channel_setup = function(channel_id, dependencies){
 	//console.log("got dependencies:", dependencies);
 	var www_server = dependencies["channel.www-server"];
-	var db_view_service = dependencies["service.database-view"]
-	www_server.route({
-        method: 'GET',
-        path: '/lolo',
-        handler: function(request, reply){
-        	console.log("captured request for nono");
-        	db_view_service.emit("list", function(data){
-        		reply(data);
-        	})
-        }
-    })
+	var db_view_service = dependencies["service.database-view"];
+
+	
+	www_server.route(
+	{
+		method: 'GET',
+		path: '/lolo',
+		handler: function(request, reply){
+			console.log("captured request for nono");
+			db_view_service.emit("list", function(data){
+				reply(data);
+			})
+		}
+	});
+	
+	www_server.route(
+	{
+		method: 'GET',
+		path: '/lolo2',
+		handler: function(request, reply){
+			console.log("captured request for nono");
+			db_view_service.emit("create", function(data){
+				reply(data);
+			})
+		}
+	});
+
 }
