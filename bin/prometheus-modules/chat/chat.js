3e346f693fe9a4da7991a22b67246785e369d353
Kuba Orlik <groovy354@gmail.com>
2014-12-06 13:44:24 +0100

    working field value validation! :)

--- bin/prometheus-modules/chat/chat.js
+++ bin/prometheus-modules/chat/chat.js
@@ -1,10 +1,6 @@
 var Service = require("prometheus-service");
 var resourceManager = require("prometheus-resource-manager");
 
-module.exports.register_services = function(){
-	return ["chat"];
-}
-
 module.exports.service_info = function(service_name){
 	var ret = {};
 	if(service_name=="chat"){
@@ -17,6 +13,11 @@ module.exports.service_info = function(service_name){
 function construct_chat_service(){
 	var chat_service = new Service();
 	//todo
+	chat_service.on("post", function(payload, callback){
+		resourceManager.newResource("chat-message", payload, function(response){
+			callback(response);
+		})
+	})
 	return chat_service;
 }
 
@@ -39,9 +40,8 @@ module.exports.construct_resource_type = function(type){
 	switch(type){
 		case "chat-message":
 			return {
-				from: {type: "reference.user"},
-				to: {type: "reference.user"},
-				message: {type: "text"},
+				from: {type: "text", required:true},//should be an association to User
+				message: {type: "text", required: true},
 				date: {type: "date"},
 				order_in_conversation: {type: "int"}
 
@@ -57,4 +57,40 @@ module.exports.construct_resource_type = function(type){
 
 module.exports.construct_associations = function(Assoc){
 	Assoc.create("chat-message", "chat-conversation", true, "is_in_conversation", "contains_messages");
+}
+
+
+/*
+	
+	channel associations
+	
+*/
+
+module.exports.channel_setup = function(channel_id, dependencies){
+	var www_server = dependencies["channel.www-server"];
+	var chat_service = dependencies["service.chat"];
+	
+	www_server.route(
+	{
+		method: 'GET',
+		path: '/chat/lolo',
+		handler: function(request, reply){
+			console.log("captured request for nono");
+			db_view_service.emit("list", function(data){
+				reply(data);
+			})
+		}
+	});
+	
+	www_server.route(
+	{
+		method: 'GET',
+		path: '/api/v1/chat/post',
+		handler: function(request, reply){
+			chat_service.emit("post", {message: "heloł mejbi", from: "groovy354@gmail.com"}, function(data){
+				reply(data);
+			})
+		}
+	});
+
 }
