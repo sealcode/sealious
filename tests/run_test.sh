ff7384d4d71b26ec2525ea3aa35c573df6625b3a
Kuba Orlik <groovy354@gmail.com>
2014-12-12 14:39:23 +0100

    changed > to &>

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,13 +4,13 @@
     echo "true"
 else
 	echo "cloning jscoverage..."
-    git clone https://github.com/visionmedia/node-jscoverage.git >/dev/null
+    git clone https://github.com/visionmedia/node-jscoverage.git &> /dev/null
     echo "    done!"
-    cd node-jscoverage/ >/dev/null
+    cd node-jscoverage/ &> /dev/null
     echo "configuring jscoverage..."
     ./configure
     echo "    done!"
     echo "installing jscoverage...."
-    make && sudo make install >/dev/null
+    make && sudo make install &> /dev/null
     echo "    done!"
 fi
