3521133cf104068a9bd735d104d8b0e4941adb1b
Kuba Orlik <groovy354@gmail.com>
2014-12-12 15:11:15 +0100

    switched to mocha for realz

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,18 +1,10 @@
 #/bin/bash
 
- if hash jscoverage 2>/dev/null; then
-    echo "true"
+if hash mocha 2>/dev/null; then
+    echo "mocha installed"
 else
-	echo "cloning jscoverage..."
-    git clone https://github.com/visionmedia/node-jscoverage.git -q 	>&-
-    echo "    done!"
-    cd node-jscoverage/ >&-
-    echo "configuring jscoverage..."
-    sudo ./configure
-    echo "    done!"
-    echo "installing jscoverage...."
-    ls -al
-    make 
-    sudo make install
-    echo "    done!"
+	echo "installing mocha"
+	sudo npm install -g mocha
 fi
+
+nodejs tests.js
