7812ec4f91ac6fb0142f561e884969ab94689308
Kuba Orlik <groovy354@gmail.com>
2014-12-12 14:36:36 +0100

    cleaned up jscoverage installer output

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,8 +3,14 @@
  if hash jscoverage 2>/dev/null; then
     echo "true"
 else
-    git clone https://github.com/visionmedia/node-jscoverage.git
-    cd node-jscoverage/
+	echo "cloning jscoverage..."
+    git clone https://github.com/visionmedia/node-jscoverage.git >/dev/null
+    echo "    done!"
+    cd node-jscoverage/ >/dev/null
+    echo "configuring jscoverage..."
     ./configure
-    make && sudo make install
+    echo "    done!"
+    echo "installing jscoverage...."
+    make && sudo make install >/dev/null
+    echo "    done!"
 fi
