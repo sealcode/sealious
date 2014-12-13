d85c1a4dee307677e4f4da9f371fb506301ec4ad
Kuba Orlik <groovy354@gmail.com>
2014-12-13 12:58:46 +0100

    silenced npm install output

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,21 +4,21 @@ if hash mocha 2>/dev/null; then
     echo "mocha installed"
 else
 	echo "installing mocha"
-	npm install -g mocha
+	npm install --silent -g mocha
 fi
 
 if hash should 2>/dev/null; then
     echo "should installed"
 else
 	echo "installing should"
-	npm install -g should
+	npm install --silent -g should
 fi
 
 if hash blanket 2>/dev/null; then
     echo "blanket installed"
 else
 	echo "installing blanket"
-	npm install -g blanket
+	npm install --silent -g blanket
 fi
 
 node tests/test.js	
