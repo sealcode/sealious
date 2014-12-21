f93935dd484a42cb83dd30d9e30a304eb3671461
Kuba Orlik <groovy354@gmail.com>
2014-12-21 15:45:54 +0100

    checkpoint

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,10 @@
 require("prometheus-exception-handler");
 var LayerManager = require("prometheus-layer-manager");
-var ModuleManager = require("prometheus-module-manager").ModuleManager;
+//var ModuleManager = require("prometheus-module-manager").ModuleManager;
 
 
 if(LayerManager.isValid()){
-	ModuleManager.bootstrap();
+	//ModuleManager.bootstrap();
 	LayerManager.init();
 } else {
 	console.log("ERR: Unknown command or layer name.")
