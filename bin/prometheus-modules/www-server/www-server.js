d69eaab07b081e4a771f87022045e0c4005eaa05
theadimar21 <adwydman@gmail.com>
2015-01-08 14:16:57 +0100

    added prometheus-module: user solving issue#46; also adde prometheus-module: rest solving issue#39

--- bin/prometheus-modules/www-server/www-server.js
+++ bin/prometheus-modules/www-server/www-server.js
@@ -13,8 +13,6 @@ ExceptionHandler.addErrorParser(function(err){
 module.exports.prepare_channel_www_server = function(channel, dispatcher, dependencies){
 	var http_channel = dependencies["channel.http"];
 
-	console.log(dependencies);
-
 	channel.server = http_channel.hapi.new_server("www", 80, {cors:true});
 	
 	channel.start = function(){
