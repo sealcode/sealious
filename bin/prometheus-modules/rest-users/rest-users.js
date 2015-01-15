4a3b49658b611f5d4d0830e118b6bc6c1d13ce63
Pola <mikolajczak.pola@gmail.com>
2015-01-15 13:11:27 +0100

    channel for adding and getting users

---
+++ bin/prometheus-modules/rest-users/rest-users.js
@@ -0,0 +1,40 @@
+module.exports.prepare_channel_rest_users = function(channel, dispatcher, dependencies){
+
+	www_server = dependencies["channel.www_server"];
+	channel.add_path = function(url, username){
+		www_server.route({
+			method: "GET",
+			path: url,
+			handler: function(request, reply){
+				dispatcher.users_get_user_data(username, dispatcher).then(function(userdata){ // wywołanie metody z dispatchera webowego
+					reply(userdata);
+				});
+			}
+			// hanlder GET ma zwrócić dane użytkownika w obiekcie JSONowym
+		});
+		www_server.route({
+			method: "POST",
+			path: url,
+			handler: function(request, reply){
+				console.log("rest.js POST", request.payload)
+				dispatcher.users_user_create(request.payload.username, request.payload.password, dispatcher).then(function(response){
+					reply(response.userdata_id.toString());
+				});
+			}
+			// handler POST ma stworzyć usera o podanej nazwie i haśle
+		});
+/*
+		www_server.route({
+			method: "DELETE",
+			path: url,
+			handler: function(request, reply){
+				console.log("rest.js DELETE", request.payload)
+				dispatcher.resources_delete(resource_type_name, request.payload).then(function(response){
+					reply();
+				});
+			}
+			// handler POST ma stworzyć zasób z podanymi wartościami
+		});
+*/
+	}	
+}
