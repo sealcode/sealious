fee5caa57b37d7c96c77f6713c060b2420db92b4
Kuba Orlik <groovy354@gmail.com>
2015-01-15 14:42:22 +0100

    small fix

--- bin/prometheus-modules/rest-users/rest-users.js
+++ bin/prometheus-modules/rest-users/rest-users.js
@@ -1,28 +1,28 @@
-module.exports.prepare_channel_rest_users = function(channel, dispatcher, dependencies){
+module.exports.prepare_channel_www_server = function(channel, dispatcher, dependencies){
 
 	www_server = dependencies["channel.www_server"];
 	url = "/api/v1/user";
-		www_server.route({
-			method: "GET",
-			path: url,
-			handler: function(request, reply){
-				dispatcher.users_get_user_data(request.payload.username, dispatcher).then(function(userdata){ // wywołanie metody z dispatchera webowego
-					reply(userdata);
-				});
-			}
-			// hanlder GET ma zwrócić dane użytkownika w obiekcie JSONowym
-		});
-		www_server.route({
-			method: "POST",
-			path: url,
-			handler: function(request, reply){
-				console.log("rest.js POST", request.payload)
-				dispatcher.users_user_create(request.payload.username, request.payload.password, dispatcher).then(function(response){
-					reply(response.userdata_id.toString());
-				});
-			}
-			// handler POST ma stworzyć usera o podanej nazwie i haśle
-		});
+	www_server.route({
+		method: "GET",
+		path: url,
+		handler: function(request, reply){
+			dispatcher.users_get_user_data(request.payload.username, dispatcher).then(function(userdata){ // wywołanie metody z dispatchera webowego
+				reply(userdata);
+			});
+		}
+		// hanlder GET ma zwrócić dane użytkownika w obiekcie JSONowym
+	});
+	www_server.route({
+		method: "POST",
+		path: url,
+		handler: function(request, reply){
+			console.log("rest.js POST", request.payload)
+			dispatcher.users_user_create(request.payload.username, request.payload.password, dispatcher).then(function(response){
+				reply(response.userdata_id.toString());
+			});
+		}
+		// handler POST ma stworzyć usera o podanej nazwie i haśle
+	});
 /*
 		www_server.route({
 			method: "DELETE",
