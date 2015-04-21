04d63075efa5b463f5a01988fa7e62d39dd8dd18
Kuba Orlik <groovy354@gmail.com>
2015-04-21 09:08:53 +0200

    Update dev-setup.sh

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,7 @@
 mkdir Sealious
 git clone https://github.com/Sealious/Sealious.git
 git clone https://github.com/Sealious/sealious-base-chips
-git clone https://github.com/Sealious/sealious-example app
+git clone https://github.com/Sealious/sealious-example-app
 npm install sealious-base-chips
 cd sealious-base-chips
 npm link .
