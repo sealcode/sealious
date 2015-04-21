f66d09d7ba0ae2739fbc6f800b0634e0602ecdfb
Kuba Orlik <groovy354@gmail.com>
2015-04-21 09:05:19 +0200

    Create dev-setup.sh

---
+++ dev-setup.sh
@@ -0,0 +1,14 @@
+mkdir Sealious
+git clone https://github.com/Sealious/Sealious.git
+git clone https://github.com/Sealious/sealious-base-chips
+git clone https://github.com/Sealious/sealious-example app
+npm install sealious-base-chips
+cd sealious-base-chips
+npm link .
+cd ../Sealious
+npm link sealious-base-chips
+npm install .
+npm link .
+cd ../sealious-example-app
+npm link sealious
+npm install
