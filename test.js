75d73ee66fe59bc61f1bf28e4038e20b8446e97a
theadimar21 <adwydman@gmail.com>
2015-04-30 21:59:59 +0200

    trying with node package request

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
 var npmCheck = require('npm-check');
+var http = require('http');
+var pkg = require('./package.json');
+var request = require('request')
 
+request("http://registry.npmjs.org/sealious/latest", function(error, response, body){
+	var sealious = JSON.parse(body);
+	if (pkg.version == sealious.version) console.log("true");
+	else console.log("false");
+})
+
+
+
+
+/*
+http.get("http://registry.npmjs.org/sealious/latest", function(res) {
+	res.on('data', function (chunk) {
+    	var string = chunk.toString();
+    	var json = JSON.parse(string);
+    	console.log(json);
+  	});
+})
+
+
+/*
 npmCheck({global: true})
 .then(function(data){
 	if (data.sealious.latest == data.sealious.installed)
@@ -7,5 +30,5 @@ npmCheck({global: true})
 	else 
 		console.log("false")
 });
-
+*/
  	
