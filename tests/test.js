8492f168dfed4a0be724372cb0c4c12057751dbe
Kuba Orlik <groovy354@gmail.com>
2015-09-02 13:22:09 +0200

    Use proper contexts in tests

--- tests/test.js
+++ tests/test.js
@@ -36,8 +36,10 @@ var never_fails_resource = new Sealious.ChipTypes.ResourceType({
 describe("Sealious", function(){
 	Sealious.start().then(function(){
 
+		var context = Sealious.Context();
+
 	    it('should not create a new resource', function(done){
-			Sealious.Dispatcher.resources.create({}, "always_fails_resource", { "#fail": "tak" })
+			Sealious.Dispatcher.resources.create(context, "always_fails_resource", { "#fail": "tak" })
 			.then(function(){
 				done(new Error("It didn't throw an error!"));
 			}).catch(function(error){
@@ -45,7 +47,7 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should create a new resource', function(done){
-			Sealious.Dispatcher.resources.create({}, "never_fails_resource", { "#success": "tak" })
+			Sealious.Dispatcher.resources.create(context, "never_fails_resource", { "#success": "tak" })
 			.then(function(result){
 				done();
 			}).catch(function(error){
@@ -53,9 +55,9 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should get data about the resource', function(done){
-			Sealious.Dispatcher.resources.create({}, "never_fails_resource", { "#success": "tak" })
+			Sealious.Dispatcher.resources.create(context, "never_fails_resource", { "#success": "tak" })
 			.then(function(result){
-				return Sealious.Dispatcher.resources.get_by_id({}, result.id)
+				return Sealious.Dispatcher.resources.get_by_id(context, result.id)
 				.then(function(resource){
 					done();	
 				})
@@ -64,9 +66,9 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should delete the resource', function(done){
-			Sealious.Dispatcher.resources.create({}, "never_fails_resource", { "#success": "tak" })
+			Sealious.Dispatcher.resources.create(context, "never_fails_resource", { "#success": "tak" })
 			.then(function(result){
-				Sealious.Dispatcher.resources.delete({}, "never_fails_resource", result.id)
+				Sealious.Dispatcher.resources.delete(context, "never_fails_resource", result.id)
 				.then(function(resource){
 					done();	
 				}).catch(function(error){
@@ -77,9 +79,9 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should update the resource', function(done){
-			Sealious.Dispatcher.resources.create({}, "never_fails_resource", { "#success": "tak" })
+			Sealious.Dispatcher.resources.create(context, "never_fails_resource", { "#success": "tak" })
 			.then(function(result){
-				Sealious.Dispatcher.resources.update_resource({}, "never_fails_resource", result.id, {"#success" : "tak2"})
+				Sealious.Dispatcher.resources.update_resource(context, "never_fails_resource", result.id, {"#success" : "tak2"})
 				.then(function(resource){
 					done();	
 				}).catch(function(error){
@@ -90,7 +92,7 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should list resources by type', function(done){
-			Sealious.Dispatcher.resources.list_by_type({}, "never_fails_resource")
+			Sealious.Dispatcher.resources.list_by_type(context, "never_fails_resource")
 			.then(function(result){
 				done();
 			}).catch(function(error){
@@ -98,7 +100,7 @@ describe("Sealious", function(){
 			});
 	 	});
 	 	it('should get resource type signature (schema)', function(done){
-			Sealious.Dispatcher.resources.get_resource_type_signature({}, "never_fails_resource")
+			Sealious.Dispatcher.resources.get_resource_type_signature(context, "never_fails_resource")
 			.then(function(result){
 				done();
 			}).catch(function(error){
