043f2308df729f74a4394f4bf539298527b7a44b
Kuba Orlik <groovy354@gmail.com>
2015-09-23 13:25:19 +0200

    Move tests to a separate dir

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,20 @@
-var Mocha = require("mocha");
-var mocha = new Mocha();
-
+var requireDir = require("require-dir");
 var fs = require("fs");
 var path = require("path");
 
 var Sealious = require("sealious"); 
 
-Sealious.test();			
+var tests = requireDir("./unit-tests");
+
+Sealious.init();
+
+for(var i in tests){
+	tests[i].test_init && tests[i].test_init();
+}
+
+Sealious.start().then(function(){
+	for(var i in tests){
+		tests[i].test_start && tests[i].test_start();
+	}
+	run();	
+})
