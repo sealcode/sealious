a1c836973c4a922aecb0d930406c24c951418faa
theadimar21 <adwydman@gmail.com>
2015-11-03 12:41:03 +0100

    new tests

--- tests/unit-tests/field-type.text.test.js
+++ tests/unit-tests/field-type.text.test.js
@@ -6,8 +6,8 @@ module.exports = {
 	test_start: function() {
 		var field_type_text = Sealious.ChipManager.get_chip("field_type", "text");		
 		describe("FieldType.Text", function() {
-			it("should return the description of the field type", function(done) {
-				if (typeof field_type_text.declaration.get_description() === "string")
+			it("should return the description of the field type", function(done) {	 
+				if (typeof field_type_text.declaration.get_description(new Sealious.Context(), {max_length: 10}) === "string")
 					done();
 				else
 					done(new Error("But it didn't"));
@@ -22,7 +22,7 @@ module.exports = {
 				})
 			});
 			it("should check if is_proper_value works correctly", function(done) {
-				field_type_text.is_proper_value(new Sealious.Context(), {max_length: 5}, 2)
+				field_type_text.is_proper_value(new Sealious.Context(), {max_length: 5}, "123")
 				.then(function() {
 					done();
 				})
