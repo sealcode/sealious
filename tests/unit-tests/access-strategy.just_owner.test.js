33118bcf192adef6366c09c5e49d37e0b37becdd
theadimar21 <adwydman@gmail.com>
2015-11-15 19:34:29 +0100

    Add AccessStrategy.JustOwner, Datastore and File tests, modify files that are being tested

---
+++ tests/unit-tests/access-strategy.just_owner.test.js
@@ -0,0 +1,10 @@
+var Sealious = require("sealious");
+
+module.exports = {
+	test_init: function() {
+
+	},
+	test_start: function() {
+		var just_owner = Sealious.ChipManager.get_chip("access_strategy", "just_owner")
+	}
+}
