57404dc22019dd37b56d84ca2e9c90fa1d6b95ad
Kuba Orlik <kontakt@jakub-orlik.pl>
2015-12-15 19:53:16 +0100

    Change coverage library to Istanbul from jscoverage to support ES6 syntax

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,50 @@
+var Sealious = require("sealious");
 var requireDir = require("require-dir");
-var fs = require("fs");
-var path = require("path");
+var Mocha = require('mocha'),
+    fs = require('fs'),
+    path = require('path');
+
+// Instantiate a Mocha instance.
+var mocha = new Mocha();
+mocha.delay();
+
+var testDir = path.resolve(module.filename, '../unit-tests')
+
+// Add each .js file to the mocha instance
+fs.readdirSync(testDir).filter(function(file){
+    // Only keep the .js files
+    return file.substr(-3) === '.js';
+
+}).forEach(function(file){
+    mocha.addFile(
+        path.join(testDir, file)
+    );
+});
 
 try{
-	fs.unlinkSync(path.resolve(module.filename, "../../db/resources"));	
+    fs.unlinkSync(path.resolve(module.filename, "../../db/resources")); 
 }catch(e){
-	console.log("Did not found `db/resources` file")
+    console.log("Did not found `db/resources` file")
 }
-
-var Sealious = require("sealious"); 
 
 var tests = requireDir("./unit-tests");
 
 Sealious.init();
 
 for(var i in tests){
-	tests[i].test_init && tests[i].test_init();
+    tests[i].test_init && tests[i].test_init();
 }
 
 Sealious.start().then(function(){
-	for(var i in tests){
-		tests[i].test_start && tests[i].test_start();
-	}
-	run();	
+    for(var i in tests){
+        tests[i].test_start && tests[i].test_start();
+    }
+    run();  
 })
+
+// Run the tests.
+mocha.run(function(failures){
+  process.on('exit', function () {
+    process.exit(failures);
+  });
+});
