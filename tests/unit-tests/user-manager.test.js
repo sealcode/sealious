22b3f5b41c528aded04c04405c8bcaa341ebc0b2
theadimar21 <adwydman@gmail.com>
2015-12-16 12:07:40 +0100

    Add tests

---
+++ tests/unit-tests/user-manager.test.js
@@ -0,0 +1,114 @@
+var Sealious = require('sealious');
+
+module.exports = {
+	test_init: function() {
+	},
+	test_start: function() {
+		describe("UserManager", function() {
+			it("creates a new user", function(done) {
+				Sealious.Dispatcher.users.create_user(new Sealious.Context(), "user", "pass")
+				.then(function(result) {
+					if (result.body.username === "user" && result.body.password === "pass") {
+						done();
+					}
+					else {
+						done(new Error("It created wrong user"))
+					}
+				})
+				.catch(function(error) {
+					done(new Error(error))
+				})
+			})
+			it("tries to create a user that already exists", function(done) {
+				Sealious.Dispatcher.users.create_user(new Sealious.Context(), "user_exists", "pass_exists")
+				.then(function(result) {
+					Sealious.Dispatcher.users.create_user(new Sealious.Context(), "user_exists", "pass_exists")
+					.then(function(result){
+						done(new Error("It created a user"));
+					})
+					.catch(function(error) {
+						if (error.type === "valueExists"){
+							done();
+						}
+						else {
+							done(new Error(error));
+						}
+					})
+				})
+				.catch(function(error) {
+						done(new Error(error))
+				});
+			});
+			it("gets all users", function(done) {
+				Sealious.Dispatcher.users.get_all_users(new Sealious.Context)
+				.then(function(result) {
+					if (typeof result === "object"){
+						if (result.length >= 2) {
+							done();
+						}
+						else {
+							done(new Error("It didn't return enough elements"));
+						}
+					}
+					else {
+						done(new Error("It didn't return an array with users"));
+					}	
+				})
+				.catch(function(error) {
+					done(new Error(error));
+				})
+			})
+			it("matched given credentials", function(done) {
+				Sealious.Dispatcher.users.password_match(new Sealious.Context(), "user_exists", "pass_exists")
+				.then(function(result) {
+					done();
+				})
+				.catch(function(error) {
+					done(new Error(error));
+				})
+			});
+			it("didn't match given credentials", function(done) {
+				Sealious.Dispatcher.users.password_match(new Sealious.Context(), "blablagarbage", "blablagarbage")
+				.then(function(result) {
+					done("It found the user");
+				})
+				.catch(function(error) {
+					if (error.type === "authorization") {
+						done();
+					}
+					else {
+						done(new Error(error));
+					}
+				})
+			})
+			it("misses username and password", function(done) {
+				Sealious.Dispatcher.users.password_match(new Sealious.Context)
+				.then(function(result) {
+					done("It worked");
+				})
+				.catch(function(error) {
+					if(error.type === "authorization"){
+						done();
+					}
+					else {
+						done(new Error(error));
+					}
+				})
+			})
+			it("misses username and password", function(done) {
+				Sealious.Dispatcher.users.password_match(new Sealious.Context, "username")
+				.then(function(result) {
+					done("It worked");
+				})
+				.catch(function(error) {
+					if(error.type === "authorization"){
+						done();
+					}
+					else {
+						done(new Error(error));
+					}
+				})
+			})
+		});
+	}
+};
