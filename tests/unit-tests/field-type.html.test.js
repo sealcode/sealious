28cfdfb601607188c72c1e67eb40c3492e508337
debian-sh <bestmatrix@gmail.com>
2016-02-15 18:04:04 +0100

    Remove naugthy strings in plain text, add blns package

--- tests/unit-tests/field-type.html.test.js
+++ tests/unit-tests/field-type.html.test.js
@@ -2,32 +2,32 @@ var Sealious = require("sealious");
 var fs = require('fs');
 
 module.exports = {
-    test_init: function() {},
-    test_start: function() {
-        var field_type_html = Sealious.ChipManager.get_chip("field_type", "html");
-        var naugthyStrings;
+	test_init: function() {},
+	test_start: function() {
+		var field_type_html = Sealious.ChipManager.get_chip("field_type", "html");
+		var naugthyStrings;
 
-        describe('FieldType.Html', function() {
-            before(function(done) {
-                naugthyStrings = fs.readFileSync(__dirname + '/naugthy-strings').toString().split("\n");
-                done();
-            })
-            it('testing array of naugthy strings against XSS attacks', function(done) {
-                describe("FieldType.Html", function() {
-                    naugthyStrings.forEach(function(string) {
-                        it('should check if is_proper_value works correctly - given naugthy string: ' + string.substr(0, 20)+"﹍", function(done) {
-                            field_type_html.is_proper_value(new Sealious.Context(), {}, string)
-                                .then(function() {
-                                    done();
-                                })
-                                .catch(function(error) {
-                                    done(new Error(error));
-                                })
-                        });
-                    });
-                });
-                done();
-            })
-        })
-    }
+		describe('FieldType.Html', function() {
+			before(function(done) {
+				naugthyStrings = require('blns');
+				done();
+			})
+			it('testing array of naugthy strings against XSS attacks', function(done) {
+				describe("FieldType.Html", function() {
+					naugthyStrings.forEach(function(string) {
+						it('should check if is_proper_value works correctly - given naugthy string: ' + string.substr(0, 20) + "﹍", function(done) {
+							field_type_html.is_proper_value(new Sealious.Context(), {}, string)
+								.then(function() {
+									done();
+								})
+								.catch(function(error) {
+									done(new Error(error));
+								})
+						});
+					});
+				});
+				done();
+			})
+		})
+	}
 };
