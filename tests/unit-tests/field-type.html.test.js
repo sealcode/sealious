42cf414b259111b36d1aa3914564ea1dcc889efb
debian-sh <bestmatrix@gmail.com>
2016-02-25 01:29:40 +0100

    Improve field type html & write tests

--- tests/unit-tests/field-type.html.test.js
+++ tests/unit-tests/field-type.html.test.js
@@ -32,7 +32,21 @@ module.exports = {
 			methods.forEach(function(method_name) {
 				it("shouldn't crash when trying to " + method_name + " a naugthy string", function(done) {
 					var promises = naugthyStrings.map(function(string) {
-						return field_type_html[method_name](new Sealious.Context(), {}, string)
+						return field_type_html[method_name](new Sealious.Context(), {
+							tags: {
+								default_decision: "remove",
+								keep: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+									'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+									'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
+								],
+								remove: []
+							},
+							attributes: {
+								default_decision: "remove",
+								keep: ['href', 'name', 'target'],
+								remove: ['src']
+							}
+						}, string)
 					});
 					Promise.all(promises)
 						.then(function() {
@@ -51,8 +65,19 @@ module.exports = {
 			}
 
 			for (prop in values) {
-				it("should check if decode works properly given '"+prop+"'", function(done) {
-					field_type_html.decode(new Sealious.Context(), {}, prop)
+				it("should check if decode works properly given '" + prop + "'", function(done) {
+					field_type_html.decode(new Sealious.Context(), {
+							tags: {
+								default_decision: "remove",
+								keep: ['a', 'h1', 'p'],
+								remove: []
+							},
+							attributes: {
+								default_decision: "remove",
+								keep: [],
+								remove: []
+							}
+						}, prop)
 						.then(function(decoded_value) {
 							if (decoded_value === values[prop]) {
 								done();
@@ -67,15 +92,21 @@ module.exports = {
 			}
 
 
-			it("should remove attributes that were not explicitly allowed (in params)", function(done) {
+			it("should remove attributes that were not explicitly declarated (in params)", function(done) {
 				field_type_html.decode(new Sealious.Context(), {
-						allowed_tags: ['a', 'p', 'h2', 'h3'],
-						allowed_attributes: {
-							a: ['id']
+						tags: {
+							default_decision: "remove",
+							keep: ['a'],
+							remove: []
+						},
+						attributes: {
+							default_decision: "remove",
+							keep: ['id'],
+							remove: []
 						}
 					}, "<a link='sth' id='fuzzelement1'>test</a>")
 					.then(function(decoded_value) {
-						if (decoded_value === "<a id=\"fuzzelement1\">test</a>") {
+						if (decoded_value === "<a id='fuzzelement1'>test</a>") {
 							done();
 						} else {
 							done(new Error("It didn't parse the value correctly"));
@@ -88,7 +119,16 @@ module.exports = {
 
 			it("should strip tags that were not explicitly allowed (in params)", function(done) {
 				field_type_html.decode(new Sealious.Context(), {
-						allowed_tags: ['a', 'p', 'h2', 'h3']
+						tags: {
+							default_decision: "remove",
+							keep: ['a', 'p', 'h2', 'h3'],
+							remove: []
+						},
+						attributes: {
+							default_decision: "remove",
+							keep: [],
+							remove: []
+						}
 					}, "<a>test</a><h4>not found</h4><h2>here is</h2>")
 					.then(function(decoded_value) {
 						if (decoded_value === "<a>test</a>not found<h2>here is</h2>") {
@@ -101,6 +141,32 @@ module.exports = {
 						done(new Error("It didn't parse the value correctly"));
 					})
 			});
+			it("shouldn't strip tags that were not explicitly allowed (in params)", function(done) {
+				field_type_html.decode(new Sealious.Context(), {
+						tags: {
+							default_decision: "keep",
+							keep: ['a', 'p', 'h3'],
+							remove: ['h4']
+						},
+						attributes: {
+							default_decision: "remove",
+							keep: [],
+							remove: ['href']
+						}
+					}, "<a>test</a><h4>not found</h4><h2>here is</h2>")
+					.then(function(decoded_value) {
+						if (decoded_value === "<a>test</a>not found<h2>here is</h2>") {
+							done();
+						} else {
+							done(new Error("It didn't parse the value correctly"));
+						}
+					})
+					.catch(function(error) {
+						done(new Error("It didn't parse the value correctly"));
+					})
+			});
+		})
+		describe('FieldType.Html - utils', function() {
 		})
 	}
 };
