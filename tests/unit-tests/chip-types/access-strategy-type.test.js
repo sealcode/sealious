c33b088a23e1e40774b4c4af22e2d56ceb9d7477
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-07-06 21:34:38 +0200

    Make the code unit-tests friendly. Refactor the tests.

---
+++ tests/unit-tests/chip-types/access-strategy-type.test.js
@@ -0,0 +1,46 @@
+var AccessStrategyType = require("lib/chip-types/access-strategy-type.js");
+var SuperContext = require("lib/super-context.js");
+var Context = require("lib/context.js");
+var assert_no_error = require("tests/util/assert-no-error.js");
+var assert = require("assert");
+
+
+describe("AccessStrategyType", function(){
+	describe(".prototype.check", function(){
+		it("should resolve if given an instance of SuperContext", function(done){
+			var sc = new SuperContext();
+			var result = AccessStrategyType.prototype.__check({}, sc);
+			assert_no_error(result, done);
+		});
+
+		it("should resolve with undefined if the strategy is item_sensitive but no item is provided", function(done){
+			AccessStrategyType.prototype.__check({item_sensitive: true}, new Context(), {})
+			.then(function(result){
+				done(assert.equal(result, undefined))
+			})
+		});
+
+		/*		it("should accept 'false' as a return value in checker_function", function(done){
+
+				});
+
+				it("should accept 'true' as a return value in checker_function", function(done){
+
+				});*/
+
+		it("should pass item as argument if the strategy is item_sensitive", function(done){
+			var item = {};
+			AccessStrategyType.prototype.__check(
+				{
+					item_sensitive: true,
+					checker_function: function(context, params, _item){
+						done(assert.equal(_item, item));
+					}
+				},
+				new Context(),
+				{},
+				item
+			)
+		});
+	});
+});
