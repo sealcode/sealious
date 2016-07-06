25102692f9fa6726db764f525c9af46d657e105f
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-07-06 21:34:38 +0200

    Make the code unit-tests friendly. Refactor the tests.

---
+++ tests/unit-tests/base-chips/access-strategy/owner.test.js
@@ -0,0 +1,29 @@
+var Context = require("lib/context.js");
+var assert_no_error = require("tests/util/assert-no-error.js");
+var assert_error = require("tests/util/assert-error.js");
+
+var owner = require("lib/base-chips/access-strategy-types/owner.js");
+
+describe("AccessStrategy.owner", function(){
+	it("checks if the user is the owner and returns true", function(done){
+		var context = new Context(undefined, "ip", "user_id");
+		var item = {
+			created_context: {
+				user_id: "user_id"
+			}
+		}
+		var result = owner.checker_function(context, {}, item);
+		assert_no_error(result, done);
+	});
+
+	it("throws if the owner id doesn't match the owner in the created_context", function(done){
+		var context = new Context(undefined, "ip", "user_id");
+		var item = {
+			created_context: {
+				user_id: "other_user_id"
+			}
+		}
+		var result = owner.checker_function(context, {}, item);
+		assert_error(result, done);
+	});
+})
