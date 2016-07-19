731f0e6b68f413cf529737cc3d2f3d069a06a839
Wydmanski <anwi@gft.com>
2016-07-19 12:09:33 +0200

    rewrite ChipManager tests to assert

--- tests/unit-tests/chip-types/chip-manager.test.js
+++ tests/unit-tests/chip-types/chip-manager.test.js
@@ -10,44 +10,43 @@ describe("ChipManager", function(){
 		}
 	};
 
-	it("gets all resource types", function(done){
+	it("gets all resource types", function(){
 		var names = ChipManager.__get_all_resource_types(chips);
-		done(assert(names.indexOf("test") > -1 && names.indexOf("test2") > -1));
-	})
+		assert.notStrictEqual(names.indexOf("test"), -1);
+		assert.notStrictEqual(names.indexOf("test2"), -1);
+	});
 
-	it("gets chip by longid (longid exists)", function(done){
+	it("gets chip by longid (longid exists)", function(){
 		var chip = ChipManager.__get_chip_by_longid(chips, "resource_type.test");
-		done(assert(chip == chips.resource_type.test));
+		assert.strictEqual(chip, chips.resource_type.test);
 	});
 
-	it("throws an error when get_chip_by_longid has non existent longid", function(done){
-		try {
-			ChipManager.__get_chip_by_longid(chips, "doesn't exist");
-			done(new Error("It didn't throw an error"));
-		}
-		catch (e) {
-			if (e.type === "validation")
-				done();
-			else
-				done(new Error(e))
-		}
+	it("throws an error when get_chip_by_longid has non existent longid", function(){
+		assert.throws(
+			function() {
+				ChipManager.__get_chip_by_longid(chips, "doesn't exist");
+			},
+			function(err) {
+				if (err.type === "validation"){
+					return true;
+				}
+			},
+			"unexpected error"
+		);
 	});
 
-	it("gets chip amount by type and returns 0 (non existent type)", function(done){
+	it("gets chip amount by type and returns 0 (non existent type)", function(){
 		var amount = ChipManager.__get_chip_amount_by_type(chips, "non existent");
-		done(assert(amount == 0));
+		assert.strictEqual(amount, 0);
 	});
 
-	it("gets chips by type (type exists)", function(done){
+	it("gets chips by type (type exists)", function(){
 		var received_chips = ChipManager.__get_chips_by_type(chips, "resource_type");
-		done(assert(received_chips.test == chips.resource_type.test));
+		assert.strictEqual(received_chips.test, chips.resource_type.test);
 	});
 
-	it("gets chips by type (type doesn't exist)", function(done){
+	it("gets chips by type (type doesn't exist)", function(){
 		var chips = ChipManager.get_chips_by_type("troll_type");
-		if (chips === undefined)
-			done();
-		else
-			done(new Error("The result is not undefined"))
+		assert.strictEqual(chips, undefined);
 	});
 })
