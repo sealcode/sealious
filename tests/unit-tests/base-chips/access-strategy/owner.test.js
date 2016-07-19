f241045d05dfafdaf7f4588cd39d7bed944efa69
Wydmanski <anwi@gft.com>
2016-07-19 13:53:44 +0200

    rewrite Owner var to const

--- tests/unit-tests/base-chips/access-strategy/owner.test.js
+++ tests/unit-tests/base-chips/access-strategy/owner.test.js
@@ -1,30 +1,30 @@
-var Context = require.main.require("lib/context.js");
-var assert = require("assert");
-var assert_no_error = require.main.require("tests/util/assert-no-error.js");
-var assert_error = require.main.require("tests/util/assert-error.js");
+const Context = require.main.require("lib/context.js");
+const assert = require("assert");
+const assert_no_error = require.main.require("tests/util/assert-no-error.js");
+const assert_error = require.main.require("tests/util/assert-error.js");
 
-var owner = require.main.require("lib/base-chips/access-strategy-types/owner.js");
+const owner = require.main.require("lib/base-chips/access-strategy-types/owner.js");
 
 describe("AccessStrategy.owner", function(){
 	it("checks if the user is the owner and returns true", function(done){
-		var context = new Context(undefined, "ip", "user_id");
-		var item = {
+		const context = new Context(undefined, "ip", "user_id");
+		const item = {
 			created_context: {
 				user_id: "user_id"
 			}
 		}
-		var result = owner.checker_function(context, {}, item);
+		const result = owner.checker_function(context, {}, item);
 		assert_no_error(result, done);
 	});
 
 	it("throws if the owner id doesn't match the owner in the created_context", function(done){
-		var context = new Context(undefined, "ip", "user_id");
-		var item = {
+		const context = new Context(undefined, "ip", "user_id");
+		const item = {
 			created_context: {
 				user_id: "other_user_id"
 			}
 		}
-		var result = owner.checker_function(context, {}, item);
+		const result = owner.checker_function(context, {}, item);
 		assert_error(result, done);
 	});
 })
