16cf6a136f0c0aba59f67d5e048640ac7f412a34
Wydmanski <anwi@gft.com>
2016-07-19 12:11:12 +0200

    PROPOSITION: rewrite var to const/let

--- tests/unit-tests/chip-types/chip-manager.test.js
+++ tests/unit-tests/chip-types/chip-manager.test.js
@@ -1,9 +1,9 @@
-var ChipManager = require.main.require("lib/chip-types/chip-manager.js");
-var assert = require("assert");
+const ChipManager = require.main.require("lib/chip-types/chip-manager.js");
+const assert = require("assert");
 
 describe("ChipManager", function(){
 
-	var chips = {
+	const chips = {
 		resource_type: {
 			test: {},
 			test2: {}
@@ -11,13 +11,13 @@ describe("ChipManager", function(){
 	};
 
 	it("gets all resource types", function(){
-		var names = ChipManager.__get_all_resource_types(chips);
+		const names = ChipManager.__get_all_resource_types(chips);
 		assert.notStrictEqual(names.indexOf("test"), -1);
 		assert.notStrictEqual(names.indexOf("test2"), -1);
 	});
 
 	it("gets chip by longid (longid exists)", function(){
-		var chip = ChipManager.__get_chip_by_longid(chips, "resource_type.test");
+		const chip = ChipManager.__get_chip_by_longid(chips, "resource_type.test");
 		assert.strictEqual(chip, chips.resource_type.test);
 	});
 
@@ -36,17 +36,17 @@ describe("ChipManager", function(){
 	});
 
 	it("gets chip amount by type and returns 0 (non existent type)", function(){
-		var amount = ChipManager.__get_chip_amount_by_type(chips, "non existent");
+		const amount = ChipManager.__get_chip_amount_by_type(chips, "non existent");
 		assert.strictEqual(amount, 0);
 	});
 
 	it("gets chips by type (type exists)", function(){
-		var received_chips = ChipManager.__get_chips_by_type(chips, "resource_type");
+		const received_chips = ChipManager.__get_chips_by_type(chips, "resource_type");
 		assert.strictEqual(received_chips.test, chips.resource_type.test);
 	});
 
 	it("gets chips by type (type doesn't exist)", function(){
-		var chips = ChipManager.get_chips_by_type("troll_type");
+		const chips = ChipManager.get_chips_by_type("troll_type");
 		assert.strictEqual(chips, undefined);
 	});
 })
