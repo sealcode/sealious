00ba86419c624460f5e0af08a0a84791a549575b
Wydmanski <anwi@gft.com>
2016-07-19 13:52:40 +0200

    rewrite SubjectPath tests to assert

--- tests/unit-tests/data-structures/subject-path.test.js
+++ tests/unit-tests/data-structures/subject-path.test.js
@@ -1,34 +1,29 @@
 var SubjectPath = require.main.require("lib/data-structures/subject-path.js");
+const assert = require("assert");
 
 describe("SubjectPath", function(){
 
-	it("can be constructed from string", function(done){
+	it("can be constructed from string", function(){
 		var d = new SubjectPath("foo.bar.baz");
-		if (d.elements[0] == "foo" && d.elements[1] == "bar" && d.elements[2] == "baz" ){
-			done();
-		} else {
-			done(new Error("It didn't parse correctly"));
-		}
+		assert.strictEqual(d.elements[0], "foo");
+		assert.strictEqual(d.elements[1], "bar");
+		assert.strictEqual(d.elements[2], "baz");
 	});
 
-	it("can be contructed from array", function(done){
+	it("can be contructed from array", function(){
 		var elements = ["foo", "bar", "baz"];
 		var d = new SubjectPath(elements);
-		if (d.elements[0] == "foo" && d.elements[1] == "bar" && d.elements[2] == "baz" ){
-			done();
-		} else {
-			done(new Error("It didn't parse correctly"));
-		}
+		assert.strictEqual(d.elements[0], "foo");
+		assert.strictEqual(d.elements[1], "bar");
+		assert.strictEqual(d.elements[2], "baz");
 	});
 
-	it("can be constructed from an instance of itself", function(done){
+	it("can be constructed from an instance of itself", function(){
 		var elements = ["foo", "bar", "baz"];
 		var d = new SubjectPath(elements);
 		var d2 = new SubjectPath(d);
-		if (d.elements[0] == "foo" && d.elements[1] == "bar" && d.elements[2] == "baz" ){
-			done();
-		} else {
-			done(new Error("It didn't parse correctly"));
-		}
+		assert.strictEqual(d2.elements[0], "foo");
+		assert.strictEqual(d2.elements[1], "bar");
+		assert.strictEqual(d2.elements[2], "baz");
 	});
 })
