2953c410b305f714efd5fefb092a1bdb36422bbd
Wydmanski <anwi@gft.com>
2016-07-20 10:46:09 +0200

    write Int tests, change Int to module.exports

--- tests/unit-tests/base-chips/field-types/email.test.js
+++ tests/unit-tests/base-chips/field-types/email.test.js
@@ -6,7 +6,7 @@ const rejectCorrectly = require.main.require("tests/util/reject-correctly.js");
 const assert = require("assert");
 
 describe("FieldType.Email", function(){
-	it("return the name of the field type", function() {
+	it("returns the name of the field type", function() {
 		assert.strictEqual(field_type_email.name, "email");
 	});
 	it("returns the description of the field type", function(){
--- tests/unit-tests/base-chips/field-types/float.test.js
+++ tests/unit-tests/base-chips/field-types/float.test.js
@@ -9,18 +9,18 @@ describe("FieldType.Float", function(){
 	it("returns the name of the field type", function() {
 		assert.strictEqual(field_type_float.name, "float");
 	});
-	it("should return the description of the field type", function(){
+	it("returns the description of the field type", function(){
 		assert.strictEqual(typeof field_type_float.get_description(), "string");
 	});
-	it("should check if is_proper_value works correctly", function(done){
+	it("checks if is_proper_value works correctly", function(done){
 		const {accept, reject} = acceptCorrectly(done);
 		field_type_float.is_proper_value(accept, reject, new Context(), {}, 2.5);
 	});
-	it("should return error because new_value is not an integer", function(done){
+	it("returns error because new_value is not an integer", function(done){
 		const {accept, reject} = rejectCorrectly(done);
 		field_type_float.is_proper_value(accept, reject, new Context(), {}, "janusz");
 	});
-	it("should check if encode works properly (given \"2\")", function(){
+	it("checks if encode works properly (given \"2\")", function(){
 		assert.strictEqual(field_type_float.encode(new Context(), {}, "2.5"), 2.5);
 	});
 });
