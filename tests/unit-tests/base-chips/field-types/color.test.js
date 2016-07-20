2e7eb33ab8fa48f05aa8a21a850404bf2fb90959
Wydmanski <anwi@gft.com>
2016-07-20 10:17:14 +0200

    change Date to module.exports, write Date tests

--- tests/unit-tests/base-chips/field-types/color.test.js
+++ tests/unit-tests/base-chips/field-types/color.test.js
@@ -6,33 +6,36 @@ const acceptCorrectly = require.main.require("tests/util/accept-correctly.js");
 const rejectCorrectly = require.main.require("tests/util/reject-correctly.js");
 
 describe("FieldType.Color", function(){
+	it("returns the name of the field type", function() {
+		assert.strictEqual(field_type_color.name, "color");
+	});
 	it("checks if is_proper_value throws an error (wrong value)", function(done){
 		const {accept, reject} = rejectCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {}, "aaaa");
+		field_type_color.is_proper_value(accept, reject, new Context(), {}, "aaaa");
 	});
 	it("checks if is_proper_value works correctly (given \"rgb(255, 255, 255)\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {}, "rgb(255, 255, 255)");
+		field_type_color.is_proper_value(accept, reject, new Context(), {}, "rgb(255, 255, 255)");
 	});
 	it("checks if is_proper_value works correctly (given \"black\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {}, "black");
+		field_type_color.is_proper_value(accept, reject, new Context(), {}, "black");
 	});
 	it("checks if is_proper_value works correctly (given \"BLACK\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {}, "BLACK");
+		field_type_color.is_proper_value(accept, reject, new Context(), {}, "BLACK");
 	});
 	it("checks if is_proper_value works correctly (given \"#000000\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {}, "#000000");
+		field_type_color.is_proper_value(accept, reject, new Context(), {}, "#000000");
 	});
 	it("checks if is_proper_value works correctly (given \"{r: 255, g: 255, b: 255}\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), {r: 255, g: 255, b: 255});
+		field_type_color.is_proper_value(accept, reject, new Context(), {r: 255, g: 255, b: 255});
 	});
 	it("checks if is_proper_value works correctly (given \"hsla(262, 59%, 81%, 0.5)\"", function(done){
 		const {accept, reject} = acceptCorrectly(done);
-		var result = field_type_color.is_proper_value(accept, reject, new Context(), "hsla(262, 59%, 81%, 0.5)");
+		field_type_color.is_proper_value(accept, reject, new Context(), "hsla(262, 59%, 81%, 0.5)");
 	});
 	it("checks if encode works correctly", function(){
 		assert.strictEqual(field_type_color.encode(new Context(), {}, {r: 0, g: 0, b: 0}), "#000000");
---
+++ tests/unit-tests/base-chips/field-types/context.test.js
@@ -0,0 +1,7 @@
+const field_type_context = require.main.require("lib/base-chips/field-types/context.js");
+const Context = require.main.require("lib/context.js");
+
+const acceptCorrectly = require.main.require("tests/util/accept-correctly.js");
+const rejectCorrectly = require.main.require("tests/util/reject-correctly.js");
+
+const assert = require("assert");
