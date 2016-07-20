781f3b4a307ecc16695a54cef7d4cb503b6096ba
Wydmanski <anwi@gft.com>
2016-07-20 11:04:40 +0200

    write Hashed-Text tests, change Hashed-Text to module.exports

--- tests/unit-tests/field-type.hashed-text.test.js
+++ tests/unit-tests/field-type.hashed-text.test.js
@@ -1,51 +1,54 @@
-var crypto = require('crypto');
+const Context = require.main.require("lib/context.js");
+const field_type_hashed_text = require.main.require("lib/base-chips/field-types/hashed-text.js");
+const acceptCorrectly = require.main.require("tests/util/accept-correctly.js");
+const rejectCorrectly = require.main.require("tests/util/reject-correctly.js");
+
+const assert = require("assert");
+const crypto = require('crypto');
 
 var assert_no_error = require("../util/assert-no-error.js");
 
-module.exports = {
-	test_start: function(){
-		var field_type_hashed_text = Sealious.ChipManager.get_chip("field_type", "hashed-text");
-		describe("FieldType.HashedText", function(){
-			it("should return the description of the field type", function(done){
-				if (typeof field_type_hashed_text.declaration.get_description() === "string")
-					done();
-				else
-					done(new Error("But it didn't"));
+describe("FieldType.HashedText", function(){
+	it("returns the name of the field type", function() {
+		assert.strictEqual(field_type_hashed_text.name, "hashed-text");
+	});
+	it("returns the field type it extends from", function() {
+		assert.strictEqual(field_type_hashed_text.extends, "text");
+	});
+	it("returns the description of the field type", function(){
+		assert.strictEqual(typeof field_type_hashed_text.get_description(), "string");
+	});
+	it("accepts given password ('pas1sw24rd1')", function(done){
+		const {accept, reject} = acceptCorrectly(done);
+		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {}, "pas1sw24rd1");
+	});
+
+	it("accepts given password ('pas1sw24rd1')", function(done){
+		const {accept, reject} = acceptCorrectly(done);
+		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {digits: 3}, "pas1sw24rd1");
+	});
+
+	it("accepts given password ('PAs1sW24rD1')", function(done){
+		const {accept, reject} = acceptCorrectly(done);
+		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {capitals: 3}, "PAs1sW24rD1");
+	});
+
+	it("accepts given password ('PaSw0rd23')", function(done){
+		const {accept, reject} = acceptCorrectly(done);
+		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {capitals: 2, digits: 3}, "PaSw0rd23");
+	});
+
+	it("resolves with a hash (algorithm: 'md5', salt: '')", function(done){
+		field_type_hashed_text.encode(new Context(), {}, "test")
+		.then(function(result){
+			crypto.pbkdf2("test", "", 4096, 64, "md5", function(err, key){
+				if (err) done(new Error(err));
+
+				key.toString('hex') === result ? done() : done(new Error("Wrong hash"))
 			});
-
-			it("accepts given password ('pas1sw24rd1')", function(done){
-				var result = field_type_hashed_text.is_proper_value(new Sealious.Context(), {}, "pas1sw24rd1");
-				assert_no_error(result, done);
-			});
-
-			it("accepts given password ('pas1sw24rd1')", function(done){
-				var result = field_type_hashed_text.is_proper_value(new Sealious.Context(), {digits: 3}, "pas1sw24rd1");
-				assert_no_error(result, done);
-			});
-
-			it("accepts given password ('PAs1sW24rD1')", function(done){
-				var result = field_type_hashed_text.is_proper_value(new Sealious.Context(), {capitals: 3}, "PAs1sW24rD1");
-				assert_no_error(result, done);
-			});
-
-			it("accepts given password ('PaSw0rd23')", function(done){
-				var result = field_type_hashed_text.is_proper_value(new Sealious.Context(), {capitals: 2, digits: 3}, "PaSw0rd23");
-				assert_no_error(result, done);
-			});
-
-			it("resolved with a hash (algorithm: 'md5', salt: '')", function(done){
-				field_type_hashed_text.encode(new Sealious.Context(), {}, "test")
-				.then(function(result){
-					crypto.pbkdf2("test", "", 4096, 64, "md5", function(err, key){
-						if (err) done(new Error(err));
-
-						key.toString('hex') === result ? done() : done(new Error("Wrong hash"))
-					});
-				})
-				.catch(function(error){
-					done(new Error(error))
-				})
-			})
 		})
-	}
-}
+		.catch(function(error){
+			done(new Error(error))
+		})
+	})
+})
