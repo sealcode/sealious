160d1575f6df6e203d365320ccfcc8532e405b06
Wydmanski <anwi@gft.com>
2016-07-21 11:04:21 +0200

    fix Hashed Text and adjust tests

--- tests/unit-tests/base-chips/field-types/hashed-text.test.js
+++ tests/unit-tests/base-chips/field-types/hashed-text.test.js
@@ -16,26 +16,31 @@ describe("FieldType.HashedText", function(){
 	it("returns the description of the field type", function(){
 		assert.strictEqual(typeof field_type_hashed_text.get_description(), "string");
 	});
+	it("returns the description of the field type with params", function() {
+		const desc = field_type_hashed_text.get_description(new Context, {digits: 4, capitals: 2});
+		assert.notStrictEqual(desc.indexOf("required digits: 4"), -1);
+		assert.notStrictEqual(desc.indexOf("required capitals: 2"), -1);
+	});
 	it("accepts given password ('pas1sw24rd1')", function(done){
 		const {accept, reject} = acceptCorrectly(done);
 		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {}, "pas1sw24rd1");
 	});
-
 	it("accepts given password ('pas1sw24rd1')", function(done){
 		const {accept, reject} = acceptCorrectly(done);
 		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {digits: 3}, "pas1sw24rd1");
 	});
-
 	it("accepts given password ('PAs1sW24rD1')", function(done){
 		const {accept, reject} = acceptCorrectly(done);
 		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {capitals: 3}, "PAs1sW24rD1");
 	});
-
 	it("accepts given password ('PaSw0rd23')", function(done){
 		const {accept, reject} = acceptCorrectly(done);
 		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {capitals: 2, digits: 3}, "PaSw0rd23");
 	});
-
+	it("rejects given password ('password')", function(done) {
+		const {accept, reject} = rejectCorrectly(done);
+		field_type_hashed_text.is_proper_value(accept, reject, new Context(), {capitals: 2, digits: 3}, "password");
+	})
 	it("resolves with a hash (algorithm: 'md5', salt: '')", function(done){
 		field_type_hashed_text.encode(new Context(), {}, "test")
 		.then(function(result){
