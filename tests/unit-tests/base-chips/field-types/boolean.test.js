9ea6274d51b9f82f1f993a58fb6eaa94dbf92000
Wydmanski <anwi@gft.com>
2016-07-21 09:23:57 +0200

    add tests to Boolean

--- tests/unit-tests/base-chips/field-types/boolean.test.js
+++ tests/unit-tests/base-chips/field-types/boolean.test.js
@@ -45,4 +45,31 @@ describe("FieldType.Boolean", function(){
 	it("encodes the value correctly (given \"true\")", function(){
 		assert.strictEqual(field_type_boolean.encode(new Context(), {}, "true"), true);
 	});
+	it("encodes the value correctly (given \"true\")", function(){
+		assert.strictEqual(field_type_boolean.encode(new Context(), {}, "true"), true);
+	});
+	it("doesn't encode the value (given 5)", function(){
+		assert.strictEqual(field_type_boolean.encode(new Context(), {}, 5), undefined);
+	});
+	it("filters to query when filter is an empty string", function() {
+		assert.deepEqual(field_type_boolean.filter_to_query(new Context(), {}, ""), {$in: [true, false]});
+	});
+	it("filters to query when filter is null", function() {
+		assert.deepEqual(field_type_boolean.filter_to_query(new Context(), {}, null), {$in: [true, false]});
+	});
+	it("filters by encoding the value \"true\"", function() {
+		assert.strictEqual(field_type_boolean.filter_to_query(new Context, {}, "true"), true);
+	});
+	it("filters by encoding the value \"false\"", function() {
+		assert.strictEqual(field_type_boolean.filter_to_query(new Context, {}, "false"), false);
+	});
+	it("filters by encoding the value \"1\"", function() {
+		assert.strictEqual(field_type_boolean.filter_to_query(new Context, {}, "1"), true);
+	});
+	it("filters by encoding the value \"0\"", function() {
+		assert.strictEqual(field_type_boolean.filter_to_query(new Context, {}, "0"), false);
+	});
+	it("filters by encoding the value 0", function() {
+		assert.strictEqual(field_type_boolean.filter_to_query(new Context, {}, 0), false);
+	});
 });
