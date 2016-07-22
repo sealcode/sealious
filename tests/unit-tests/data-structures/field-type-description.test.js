fa85f77b89cb506f9eae9b075ed8253f7e287f60
adwydman <adwydman@gmail.com>
2016-07-22 15:05:45 +0200

    Add tests to FieldTypeDescrption

---
+++ tests/unit-tests/data-structures/field-type-description.test.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+const FieldTypeDescription = require.main.require("lib/data-structures/field-type-description.js");
+
+describe("Sealious.FieldTypeDescription", function() {
+    it("returns a FieldTypeDescription object", function() {
+        const ftd = new FieldTypeDescription("a", "b", "c");
+        assert.strictEqual(ftd.summary, "a");
+        assert.strictEqual(ftd.raw_params, "b");
+        assert.strictEqual(ftd.extra_info, "c");
+
+        const ftd2 = new FieldTypeDescription("a", "b");
+        assert.deepEqual(ftd2.extra_info, {});
+    });
+});
