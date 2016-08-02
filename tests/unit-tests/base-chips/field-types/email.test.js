e5352be336f63823a3d7d8170df87027c480ee69
adwydman <adwydman@gmail.com>
2016-08-02 10:57:40 +0200

    Rewrite deconstructors

--- tests/unit-tests/base-chips/field-types/email.test.js
+++ tests/unit-tests/base-chips/field-types/email.test.js
@@ -6,18 +6,20 @@ const rejectCorrectly = require.main.require("tests/util/reject-correctly.js");
 const assert = require("assert");
 
 describe("FieldType.Email", function(){
-	it("returns the name of the field type", function() {
+	it("returns the name of the field type", function(){
 		assert.strictEqual(field_type_email.name, "email");
 	});
 	it("returns the description of the field type", function(){
 		assert.strictEqual(typeof field_type_email.get_description(), "string");
 	});
 	it("checks if is_proper_value works correctly(given correct date format)", function(done){
-		const {accept, reject} = acceptCorrectly(done);
+		const accept = acceptCorrectly(done).accept;
+		const reject = acceptCorrectly(done).reject;
 		field_type_email.is_proper_value(accept, reject, new Context(), {}, "test@mail.com");
 	});
 	it("checks if is_proper_value works correctly(given incorrect date format)", function(done){
-		const {accept, reject} = rejectCorrectly(done);
+		const accept = rejectCorrectly(done).accept;
+		const reject = rejectCorrectly(done).reject;
 		field_type_email.is_proper_value(accept, reject, new Context(), {}, "test");
 	});
 });
--- tests/unit-tests/base-chips/field-types/file.test.js
+++ tests/unit-tests/base-chips/field-types/file.test.js
@@ -9,26 +9,30 @@ const assert = require("assert");
 describe("FieldType.File", function(){
 	it("returns undefined", function(){
 		assert.strictEqual(field_type_file.is_proper_value({}, {}, new Context(), {}, undefined), undefined);
-	});;
+	});
 	it("accepts given value (which is an instance of Sealious.File)", function(done){
-		const {accept, reject} = acceptCorrectly(done);
+		const accept = acceptCorrectly(done).accept;
+		const reject = acceptCorrectly(done).reject;
 		field_type_file.is_proper_value(accept, reject, new Context(), {}, new Sealious.File());
-	});;
+	});
 	it("accepts given value (which is an object with filename and data attributes)", function(done){
 		const value = {
 			filename: "test_filename",
 			data: new Buffer(1)
 		};
-		const {accept, reject} = acceptCorrectly(done);
+		const accept = acceptCorrectly(done).accept;
+		const reject = acceptCorrectly(done).reject;
 		field_type_file.is_proper_value(accept, reject, new Context(), {}, value);
 	});
 	it("rejects given value (which is an empty object)", function(done){
-		const {accept, reject} = rejectCorrectly(done);
+		const accept = rejectCorrectly(done).accept;
+		const reject = rejectCorrectly(done).reject;
 		field_type_file.is_proper_value(accept, reject, new Context(), {}, {});
 	});
 	it("rejects given value (which is an array)", function(done){
-		const {accept, reject} = rejectCorrectly(done);
-		field_type_file.is_proper_value(accept, reject, new Context(), {}, [])
+		const accept = rejectCorrectly(done).accept;
+		const reject = rejectCorrectly(done).reject;
+		field_type_file.is_proper_value(accept, reject, new Context(), {}, []);
 	});
 	it("checks if encode works correctly (value_in_code is false)", function(){
 		assert.strictEqual(field_type_file.encode(new Context(), {}, false), null);
