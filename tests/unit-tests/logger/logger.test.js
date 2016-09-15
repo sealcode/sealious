cd81e0f2ccfe6b8974acf904df355f6f24de9664
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-09-15 13:55:10 +0200

    Change config key names for logger

--- tests/unit-tests/logger/logger.test.js
+++ tests/unit-tests/logger/logger.test.js
@@ -7,13 +7,13 @@ const ConfigManager = locreq("lib/config/config-manager.js");
 
 describe("Sealious.Logger", function(){
     it("gets default config of Logger", function(){
-        const config = ConfigManager.get_config().logger_config;
-        assert.strictEqual(config.logger_level, "info");
-        assert.strictEqual(config.logger_color, true);
-        assert.strictEqual(config.logger_file, null);
-        assert.strictEqual(config.logger_dirname, ".");
-        assert.strictEqual(config.logger_to_json, false);
-        assert.strictEqual(config.logger_rotation, ".yyyy-MM-Tdd");
+        const config = ConfigManager.get_config().logger;
+        assert.strictEqual(config.level, "info");
+        assert.strictEqual(config.color, true);
+        assert.strictEqual(config.file, null);
+        assert.strictEqual(config.dirname, ".");
+        assert.strictEqual(config.to_json, false);
+        assert.strictEqual(config.rotation, ".yyyy-MM-Tdd");
     });
     it("checks if levels are correct", function(){
         assert.strictEqual(Logger.levels.lazyseal, 0);
