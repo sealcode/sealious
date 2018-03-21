607c40b776afca0907b173ecf5756a91979b230f
Kuba Orlik <kontakt@kuba-orlik.name>
2018-03-21 17:35:04 +0100

    Registration-intent flow
    Summary: Registration-intent flow, better i18n
    
    Test Plan: Run automated tests. To see the html endpoints in action, comment out 'app.stop' in setup-test.js and go through the process manually
    
    Reviewers: piotr-ptaszynski, bartosz-gordon, Stanislaw-Golebiewski
    
    Reviewed By: piotr-ptaszynski
    
    Differential Revision: https://hub.sealcode.org/D193

--- setup-test.js
+++ setup-test.js
@@ -1,6 +1,10 @@
 const Sealious = require("./lib/main.js");
 const locreq = require("locreq")(__dirname);
 
+const axios = require("axios");
+const axiosCookieJarSupport = require("@3846masa/axios-cookiejar-support");
+axiosCookieJarSupport(axios);
+
 before(async () => {
 	global.TestApp = new Sealious.App(
 		{
