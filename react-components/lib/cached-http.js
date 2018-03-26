a84124a096e1dbf363590996f2bc62e9c0be0c17
Bartosz Gordon <bartosz.gordon@sealcode.org>
2018-03-26 06:31:54 +0200

    Add react-components and ResourceSelect.jsx
    Summary: Ref T765
    
    Test Plan:
    Trudno to przetestować w zgrabny sposób, ale ja to zrobiłem następująco: w ejko w docker-compose dodałem wolumen, który podmienia sealiousa w
    tamtym kontenerze na lokalną wersję sealiousa, zbudowałem apkę i Resource Select zadziałał.
    
    Reviewers: #reviewers, kuba-orlik
    
    Reviewed By: #reviewers, kuba-orlik
    
    Maniphest Tasks: T765
    
    Differential Revision: https://hub.sealcode.org/D198

---
+++ react-components/lib/cached-http.js
@@ -0,0 +1,79 @@
+const axios = require("axios");
+const CancelToken = axios.CancelToken;
+const url = require("url");
+const merge = require("merge");
+const Promise = require("bluebird");
+Promise.config({ cancellation: true });
+
+function CachedError() {}
+
+CachedError.prototype = Error.prototype;
+
+function respond_from_cache(value) {
+	return Promise[value instanceof CachedError ? "reject" : "resolve"](value);
+}
+
+const CachedHttp = (function() {
+	let cache = {};
+
+	var pending = {};
+
+	return {
+		get: function(url_arg, query, options) {
+			const parsed_url = url.parse(url_arg, true);
+			delete parsed_url.search;
+			const merged_query = merge(true, parsed_url.query, query);
+			const pathname = parsed_url.pathname;
+
+			const hash = pathname + "|" + JSON.stringify(merged_query);
+
+			if (pending[hash]) {
+				return Promise.resolve(pending[hash]).finally(() =>
+					respond_from_cache(cache[hash])
+				);
+			} else if (cache[hash]) {
+				return respond_from_cache(cache[hash]);
+			} else {
+				const promise = new Promise((resolve, reject, onCancel) => {
+					const source = CancelToken.source();
+					const qp = axios
+						.get(
+							pathname,
+							Object.assign({}, merged_query, {
+								cancelToken: source.token,
+								options,
+							})
+						)
+						.then(response => {
+							const data = response.data;
+							cache[hash] = response.data;
+							delete pending[hash];
+							resolve(data);
+						})
+						.catch(error => {
+							const cached_err = Object.create(
+								CachedError.prototype
+							);
+							Object.assign(cached_err, error);
+							delete pending[hash];
+							cache[hash] = cached_err;
+							reject(cached_err);
+						});
+					onCancel(function() {
+						delete cache[hash];
+						delete pending[hash];
+						source.cancel();
+					});
+				});
+				pending[hash] = promise.then(respond_from_cache);
+				return promise;
+			}
+		},
+		flush: function() {
+			//resets cache
+			cache = {};
+		},
+	};
+})();
+
+module.exports = CachedHttp;
