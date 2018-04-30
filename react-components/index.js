34179059139ea1906ebd5c3daa21ab3d5746c6af
Kuba Orlik <kontakt@kuba-orlik.name>
2018-04-30 22:03:23 +0200

    Fix incorrect require in s/r-c

--- react-components/index.js
+++ react-components/index.js
@@ -3,6 +3,6 @@ module.exports = {
 	Collection: require("./lib/collection.jsx"),
 	Resource: require("./lib/resource.jsx"),
 	QueryStores: require("./lib/query-stores/query-store.js"),
-	Loading: require("./lib/loading.js"),
-	CachedHttp: require("./lib/cached-http.js")
+	Loading: require("./lib/loading.jsx"),
+	CachedHttp: require("./lib/cached-http.js"),
 };
