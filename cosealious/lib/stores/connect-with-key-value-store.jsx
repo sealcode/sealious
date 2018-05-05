3fb6752927e038705192be2b6ca025b512590571
Kuba Orlik <kontakt@kuba-orlik.name>
2018-05-05 18:58:29 +0200

    Fix name conflict with sealious-react-components when bundled by parcel
    Summary: Ref T900
    
    Test Plan: Połączyłem to z Ejko ze spatchowanym D251. Odpaliłem 'make build' i przeklikałem cały front
    
    Reviewers: #reviewers, piotr-ptaszynski
    
    Reviewed By: #reviewers, piotr-ptaszynski
    
    Subscribers: piotr-ptaszynski
    
    Maniphest Tasks: T900
    
    Differential Revision: https://hub.sealcode.org/D250

--- cosealious/lib/stores/connect-with-key-value-store.jsx
+++ cosealious/lib/stores/connect-with-key-value-store.jsx
@@ -2,11 +2,12 @@ const React = require("react");
 const merge = require("merge");
 
 module.exports = function(store, store_prop_name, component) {
-	return React.createClass({
-		getInitialState: function() {
-			return { [store_prop_name]: {} };
-		},
-		componentDidMount: function() {
+	return class extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = { [store_prop_name]: {} };
+		}
+		componentDidMount() {
 			const self = this;
 			const listener = function(value) {
 				self.setState({ [store_prop_name]: value });
@@ -16,18 +17,18 @@ module.exports = function(store, store_prop_name, component) {
 				listener: listener,
 				[store_prop_name]: store.getStore(),
 			});
-		},
-		componentWillUnmount: function() {
+		}
+		componentWillUnmount() {
 			const self = this;
 			store.off("change", self.state.listener);
-		},
-		render: function() {
+		}
+		render() {
 			return React.createElement(
 				component,
 				merge(true, this.props, {
 					[store_prop_name]: this.state[store_prop_name],
 				})
 			);
-		},
-	});
+		}
+	};
 };
