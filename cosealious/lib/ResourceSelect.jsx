3fe5fe1cd5246830af66415a1d8278c7a68160c6
Kuba Orlik <kontakt@kuba-orlik.name>
2018-05-16 13:44:46 +0200

    Rename ResourceSelect ro ResourceDropdown to work around a weird parcel bug

--- cosealious/lib/ResourceSelect.jsx
+++
@@ -1,4 +0,0 @@
-import resourceTypeCollection from "./mixins/resourceTypeCollection.jsx";
-import ResourceSelectPure from "./ResourceSelectPure.jsx";
-
-module.exports = resourceTypeCollection(ResourceSelectPure);
