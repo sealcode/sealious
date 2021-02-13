ccb515f874e232c707e601e0c84c8df9e422d40a
Kuba Orlik <kontakt@kuba-orlik.name>
2021-02-13 12:17:06 +0100

    Add MatchPolicy middleware

---
+++ src/http/match-policy.ts
@@ -0,0 +1,18 @@
+import { Middleware } from "koa";
+import Policy from "../chip-types/policy";
+
+export default function MatchPolicy(policy: Policy): Middleware {
+	return async function (ctx, next) {
+		const result = await policy.check(ctx.$context);
+		if (!result) {
+			ctx.body = "Not allowed";
+			ctx.status = 403;
+			return;
+		}
+		if (!result.allowed) {
+			ctx.body = result.reason;
+			ctx.status = 403;
+		}
+		await next();
+	};
+}
--- src/http/middlewares.ts
+++ src/http/middlewares.ts
@@ -1,4 +1,5 @@
 import { default as extractContext } from "./extract-context";
 import { default as parseBody } from "./parse-body";
+import { default as MatchPolicy } from "./match-policy";
 
-export default { extractContext, parseBody };
+export default { extractContext, parseBody, MatchPolicy };
