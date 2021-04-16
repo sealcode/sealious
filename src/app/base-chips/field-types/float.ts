ffb207cc51e59318708440bc9cd4b5e892dae35b
brtkuu <bartoszposert5@gmail.com>
2021-04-16 19:46:21 +0200

    Add range filters to float

--- src/app/base-chips/field-types/float.ts
+++ src/app/base-chips/field-types/float.ts
@@ -1,4 +1,5 @@
-import { Context, Field } from "../../../main";
+import { Context, Errors, Field } from "../../../main";
+import humanComparatorToQuery, { ComparatorObject, DBComparator, HumanComparator } from "../../../utils/human-comparator-to-query";
 
 /** Stores a floating point number. DOes not take params. Does not allow range filtering. @todo: add range filtering */
 export default class Float extends Field {
@@ -10,6 +11,33 @@ export default class Float extends Field {
 		} else {
 			return Field.valid();
 		}
+	}
+	async filterToQuery(
+		_: Context,
+		field_filter: number | string | ComparatorObject<number>
+	) {
+		if (typeof field_filter !== "object") {
+			return {
+				$eq: parseInt(field_filter.toString(), 10),
+			};
+		}
+		// treating filter as a query here
+		const new_filter: { [db_comp in DBComparator]?: number } = {};
+		for (const comparator in field_filter) {
+			const new_comparator = humanComparatorToQuery(
+				comparator as HumanComparator
+			);
+			if (new_comparator === undefined) {
+				throw new Errors.ValidationError(
+					`Unknown comparator: '${comparator}'.`
+				);
+			}
+			new_filter[new_comparator] = parseInt(
+				field_filter[comparator as HumanComparator].toString(),
+				10
+			);
+		}
+		return new_filter;
 	}
 	async encode(_: any, value: number | string) {
 		if (value === null) {
