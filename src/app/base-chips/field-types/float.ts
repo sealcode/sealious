3d17c243ff9d6989f6a144d0b85770898dd82240
Kuba Orlik <kontakt@kuba-orlik.name>
2021-04-17 11:28:47 +0200

    Fix float filter to use parseFloat

--- src/app/base-chips/field-types/float.ts
+++ src/app/base-chips/field-types/float.ts
@@ -1,10 +1,18 @@
+import { ValidationResult } from "../../../chip-types/field";
 import { Context, Errors, Field } from "../../../main";
-import humanComparatorToQuery, { ComparatorObject, DBComparator, HumanComparator } from "../../../utils/human-comparator-to-query";
+import humanComparatorToQuery, {
+	ComparatorObject,
+	DBComparator,
+	HumanComparator,
+} from "../../../utils/human-comparator-to-query";
 
 /** Stores a floating point number. DOes not take params. Does not allow range filtering. @todo: add range filtering */
 export default class Float extends Field {
 	typeName = "float";
-	async isProperValue(context: Context, input: number) {
+	async isProperValue(
+		context: Context,
+		input: number
+	): Promise<ValidationResult> {
 		const test = parseFloat(input.toString());
 		if (test === null || isNaN(test) || isNaN(input) === true) {
 			return Field.invalid(context.app.i18n("invalid_float", [input]));
@@ -12,13 +20,14 @@ export default class Float extends Field {
 			return Field.valid();
 		}
 	}
+
 	async filterToQuery(
 		_: Context,
 		field_filter: number | string | ComparatorObject<number>
 	) {
 		if (typeof field_filter !== "object") {
 			return {
-				$eq: parseInt(field_filter.toString(), 10),
+				$eq: parseFloat(field_filter.toString(), 10),
 			};
 		}
 		// treating filter as a query here
@@ -32,13 +41,14 @@ export default class Float extends Field {
 					`Unknown comparator: '${comparator}'.`
 				);
 			}
-			new_filter[new_comparator] = parseInt(
+			new_filter[new_comparator] = parseFloat(
 				field_filter[comparator as HumanComparator].toString(),
 				10
 			);
 		}
 		return new_filter;
 	}
+
 	async encode(_: any, value: number | string) {
 		if (value === null) {
 			return null;
