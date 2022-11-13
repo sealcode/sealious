1ad3b18baf0cd147b74ca70b9cd1b2b25a6eb1b8
Kuba Orlik <kontakt@kuba-orlik.name>
2022-11-13 11:21:12 +0100

    Remove content-disposition header in downloaded files

--- src/http/routes/uploaded-files.ts
+++ src/http/routes/uploaded-files.ts
@@ -4,10 +4,13 @@ const uploaded_files = new Router();
 uploaded_files.get("/:id/:filename", async (ctx) => {
 	const file = await File.fromID(ctx.$app, ctx.params.id);
 	ctx.body = file.getStream();
-	ctx.set(
-		"Content-Disposition",
-		`attachment; filename*=utf-8''="${encodeURIComponent(file.filename)}"`
-	);
+	// removing this as it only created trouble and you can declare download
+	// target in html <a> element, anyway
+
+	// ctx.set(
+	// 	"Content-Disposition",
+	// 	`attachment; filename*=utf-8''="${encodeURIComponent(file.filename)}"`
+	// );
 });
 
 export default uploaded_files;
