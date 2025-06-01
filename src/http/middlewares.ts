59cabf4ae640f1c37fa88414560caab918723833
Kuba Orlik <kontakt@kuba-orlik.name>
2025-06-01 15:40:42 +0200

    Export the handleError middleware

--- src/http/middlewares.ts
+++ src/http/middlewares.ts
@@ -1,5 +1,6 @@
 import { default as extractContext } from "./extract-context.js";
 import { default as parseBody } from "./parse-body.js";
 import { default as MatchPolicy } from "./match-policy.js";
+import { default as handleError } from "./handle-error.js";
 
-export default { extractContext, parseBody, MatchPolicy };
+export default { extractContext, parseBody, MatchPolicy, handleError };
